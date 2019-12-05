7a76151b8c4ed1f9569151dd763ebaec6da43585
Mikey Williams <manicdesigner@gmail.com>
2019-12-05 12:35:46 -0500

    remove user deleted remove user id no exist

--- index.js
+++ index.js
@@ -34,28 +34,41 @@ app.get('/api/users/:id', (req, res) => {
   const id = req.params.id;
 
   db.findById(id).then(response => {
-    res
-      .status(200)
-      .json({
-        url: `/api/users/${id}`,
-        user: response
-      })
-      .catch(err => {
-        res.status(500).json({ message: 'An error occured...' });
-      });
+    if (req.params.id === id) {
+      res
+        .status(200)
+        .json({
+          url: `/api/users/${id}`,
+          user: response
+        })
+    } else if (req.params.id !== id) {
+      res.status(404).json({ message: "The user with the specified ID does not exist." })
+    } else {
+      res.status(500).json({ errorMessage: "The user information could not be modified." })
+    }
   });
 });
 
 app.delete('/api/users/:id', (req, res) => {
   const id = req.params.id;
 
-  db.findById(id)
+  db.remove(id)
     .then(response => {
-      res.status(200).json({
-        url: `/api/users/${id}`
-      })
+      if (response === id) {
+        return res.status(200).json({ message: 'User Deleted' });
+      }
+    })
+    .then(response => {
+      if (response !== id) {
+        return res.status(404).json({ message: "The user with the specified ID does not exist." })
+      }
+
+    })
+    .catch(err => {
+      res.status(500).json({ message: "The user could not be removed." })
     })
 })
+
 
 app.listen(port, host, () => {
   console.log(`server listening on http://${host}:${port}`);
